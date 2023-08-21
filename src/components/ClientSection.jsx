be20aa8fd66dd38ecb8496e4b92bd5d0a145f303
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2023-08-21 23:40:07 +0100

    client section updated

--- src/components/ClientSection.jsx
+++ src/components/ClientSection.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import styles from "./ClientSection.module.css"
+import styles from "./ClientSection.module.css";
+import videoImage from "../assets/images/col-md-6.png"
+import greaterThan from '../assets/images/@none.png'
+
 
 export default function ClientSection() {
   return (
     <section className={styles.clientSection}>
-      <div>
-        hello
-      </div>
+        <article className={styles.clientSectionLeft} >
+          <div className={styles.line}></div>
+          <h2>Watch our Courses</h2>
+          <p>Problems trying to resolve the conflict between the two major realms of Classical physics: 
+            Newtonian mechanics </p>
+          <p className={styles.learnMore}>Learn More <span><img src={greaterThan} alt="" /></span></p>
+        </article>
+      <img src={videoImage} alt="videoImage" />
  </section>
   )
 }
