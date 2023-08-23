137fc55c93b6e29ef75b0896580d60eeda907152
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2023-08-23 10:51:58 +0100

    resolved

--- src/components/ConnectSection.jsx
+++ src/components/ConnectSection.jsx
@@ -4,14 +4,18 @@ import User1 from "../assets/images/user-cover-4.png"
 import User2 from "../assets/images/fixed-height sec-.png"
 import User3 from "../assets/images/user-cover-1.png"
 import User4 from "../assets/images/user-cover-2.png"
+import styles from "./ConnectSection.module.css"
 
 export default function ConnectSection() {
   return (
-    <div className={styles}>
+    <div className={styles.ConnectSection}>
+
+      <div className={styles.users}>
           <ConnectCard cardImage={ User1} cardName={"Julian Jameson"}/>
           <ConnectCard cardImage={ User2} cardName={"Julian Jameson"}/>
           <ConnectCard cardImage={ User3} cardName={"Julian Jameson"}/>
-          <ConnectCard cardImage={ User4} cardName={"Julian Jameson"}/>
+        <ConnectCard cardImage={User4} cardName={"Julian Jameson"} />
+        </div>
     </div>
   )
 }
